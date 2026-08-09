8e57024c4d6099e40b75c90d369e5b30aa12475d
flaimes313-source <flaimes313@gmal.com>
2026-08-09 09:09:15 +0300

    f

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,2 +1,28 @@
 # keyboards/__init__.py
-from .menu import main_menu, stats_menu, direction_keyboard, result_keyboard, mistake_keyboard, add_trade_menu
+from .menu import (
+    router,
+    main_menu,
+    stats_menu,
+    deposit_menu,
+    confirm_reset_deposit_menu,
+    direction_keyboard,
+    result_keyboard,
+    mistake_keyboard,
+    add_trade_menu,
+    open_trades_menu,
+    confirm_delete_menu
+)
+
+__all__ = [
+    'router',
+    'main_menu',
+    'stats_menu',
+    'deposit_menu',
+    'confirm_reset_deposit_menu',
+    'direction_keyboard',
+    'result_keyboard',
+    'mistake_keyboard',
+    'add_trade_menu',
+    'open_trades_menu',
+    'confirm_delete_menu'
+]
